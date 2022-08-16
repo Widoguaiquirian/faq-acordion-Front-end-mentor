4c7c6ada04b97046d53a267660b6296d07afabb7
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-08-16 07:06:45 -0400

    Improving the javaScript

--- design/script.js
+++ design/script.js
@@ -2,18 +2,17 @@
 // ! FOR LESSONS NAMES //
 // ? FOR REGULAR COMMENTS //
 
-// const btns = document.querySelectorAll(".btn");
-
-// btns.forEach(function (btn) {
-//    btn.addEventListener("click", function () {
-//       btn.parentNode.classList.toggle("active");
-//    });
-// });
-
 const containers = document.querySelectorAll(".question-container");
 
 containers.forEach(function (container) {
    container.addEventListener("click", function () {
-      container.classList.toggle("active");
+      removeACtive();
+      container.classList.add("active");
    });
 });
+
+const removeACtive = function () {
+   containers.forEach(function (container) {
+      container.classList.remove("active");
+   });
+};
